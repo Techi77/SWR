47f6ab4f1ee40d7484f003db29230c456d390070
Katharine Rossinskaya <rossinskaya98@gmail.com>
2021-10-06 18:09:38 +0300

    feat: add view before ScrollView

--- src/screens/profile/ProfileScreen.js
+++ src/screens/profile/ProfileScreen.js
@@ -1,52 +1,30 @@
 // modules
-import React, {useEffect} from "react";
-import {Text, View, ScrollView} from "react-native";
+import React from "react";
+import {Text, View, ScrollView, TouchableOpacity} from "react-native";
 import {copilot, walkthroughable, CopilotStep} from "react-native-copilot";
 
 
-class Block extends React.Component {
-  render() {
-    return (
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          borderWidth: 2,
-          height: 200,
-          width: 200,
-          marginTop: 20
-        }}>
-        <Text style={{fontSize: 32}}>{this.props.blockNum}</Text>
-      </View>
-    );
-  }
-}
-
-const CopilotBlock = walkthroughable(Block);
 const CopilotView = walkthroughable(View);
 
 class ProfileScreen extends React.Component {
 
-  componentDidMount() {
-    this.props.start(false, this.scrollView);
-  }
+  
 
   render() {
     return (
       <View style={{justifyContent: "center", alignItems: "center", width: "100%", marginBottom: 50}}>
-        <ScrollView ref={(ref) => (this.scrollView = ref)} style={{height: "100%" }}>
-          <CopilotStep
+        <CopilotStep
             text="This is block #1"
             order={1}
             name="block1"
           >
             <CopilotView style={{height: 250, width: 250, borderWidth: 2, marginTop: 20}}>
-              <Text>
-               Block 1
-              </Text>
+            <TouchableOpacity onPress={() => this.props.start(false, this.scrollView)}>
+            <Text >{`Начать ${"\n"}обучение`}</Text>
+          </TouchableOpacity>
             </CopilotView>
           </CopilotStep>
-
+        <ScrollView ref={(ref) => (this.scrollView = ref)} >
           <CopilotStep
             text="This is block #2"
             order={2}
@@ -54,11 +32,10 @@ class ProfileScreen extends React.Component {
           >
             <CopilotView style={{height: 250, width: 250, borderWidth: 2, marginTop: 20}}>
               <Text>
-                Block 2
+               Block 2
               </Text>
             </CopilotView>
           </CopilotStep>
-
 
           <CopilotStep
             text="This is block #3"
@@ -72,8 +49,9 @@ class ProfileScreen extends React.Component {
             </CopilotView>
           </CopilotStep>
 
+
           <CopilotStep
-            text="This is block #4"
+            text="This is block #3"
             order={4}
             name="block4"
           >
@@ -96,6 +74,18 @@ class ProfileScreen extends React.Component {
             </CopilotView>
           </CopilotStep>
 
+          <CopilotStep
+            text="This is block #6"
+            order={6}
+            name="block6"
+          >
+            <CopilotView style={{height: 250, width: 250, borderWidth: 2, marginTop: 20}}>
+              <Text>
+                Block 6
+              </Text>
+            </CopilotView>
+          </CopilotStep>
+
         </ScrollView>
       </View>
     );
@@ -103,7 +93,12 @@ class ProfileScreen extends React.Component {
 }
 
 
-export default copilot()(ProfileScreen);
+export default copilot({
+  overlay: "svg",
+  animated: true,
+  stopOnOutsideClick: true,
+  verticalOffset: null}
+)(ProfileScreen);
 
 
 
